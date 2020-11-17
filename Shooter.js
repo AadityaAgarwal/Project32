293fb6af4246a5fdc2f814c0921c337b1e584f90
AadityaAgarwal <73215428+AadityaAgarwal@users.noreply.github.com>
2020-11-17 11:32:14 +0530

    Add files via upload

---
+++ Shooter.js
@@ -0,0 +1,19 @@
+class Shooter{
+    constructor(x, y, radius) {
+       this.radius = radius;
+       this.x = x;
+       this.y = y;
+        this.body = Bodies.circle(this.x, this.y, this.radius);
+        //this.img= loadImage("untitled.png");
+        
+       
+        World.add(world, this.body);
+      }
+      display(){
+        var spos = this.body.position;
+        fill("yellow");
+        ellipseMode(CENTER);
+        circle(spos.x,spos.y,this.radius);
+        
+      }
+}
